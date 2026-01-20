a56884156460393dd46d7575e39a64fee42eedd6
luckly <chenlin1037@gmail.com>
2026-01-21 00:31:26 +0800

    Initial Commit

---
+++ Whisper/WhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperApp.swift
+//  Whisper
+//
+//  Created by luckly on 2026/1/21.
+//
+
+import SwiftUI
+
+@main
+struct WhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhisperTests/WhisperTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperTests.swift
+//  WhisperTests
+//
+//  Created by luckly on 2026/1/21.
+//
+
+import Testing
+@testable import Whisper
+
+struct WhisperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
