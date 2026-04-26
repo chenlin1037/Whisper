9fafe0ee06d5e53c33c65188f8621b40b307d645
luckly <chenlin1037@gmail.com>
2026-04-26 21:13:34 +0800

    Initial Commit

---
+++ WhiteNoisePlayer/WhiteNoisePlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteNoisePlayerApp.swift
+//  WhiteNoisePlayer
+//
+//  Created by luckly on 2026/4/26.
+//
+
+import SwiftUI
+
+@main
+struct WhiteNoisePlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhiteNoisePlayerTests/WhiteNoisePlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteNoisePlayerTests.swift
+//  WhiteNoisePlayerTests
+//
+//  Created by luckly on 2026/4/26.
+//
+
+import Testing
+@testable import WhiteNoisePlayer
+
+struct WhiteNoisePlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
